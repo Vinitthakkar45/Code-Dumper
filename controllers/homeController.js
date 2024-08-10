730bd86da6bc505b7983106055711395326161b2
Vinitthakkar45 <45vinitthakkar@gmail.com>
2024-08-10 18:59:13 +0530

    change title

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,7 @@
 import { blogPosts } from './newPostController.js';
 
 const homeController = (req, res) => {
-    res.render('./pages/index',{ title: 'Code Ditcher' , blogPosts: blogPosts});
+    res.render('./pages/index',{ title: 'Code Dumper' , blogPosts: blogPosts});
 }
 
 export {homeController};
